428e89b1cc8caa0e0710b201cbd5daa433f64bc3
Reid Draper <reiddraper@gmail.com>
2012-04-21 11:52:23 -0400

    first commit

---
+++ src/crosscram_sample/core.clj
@@ -0,0 +1,1 @@
+(ns crosscram-sample.core)
---
+++ test/crosscram_sample/test/core.clj
@@ -0,0 +1,6 @@
+(ns crosscram-sample.test.core
+  (:use [crosscram-sample.core])
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
